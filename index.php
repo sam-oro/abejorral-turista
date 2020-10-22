52b78cae4e6feea8546dfa8907532f5eb159ac8c
sam-oro <samiroroz@gmail.com>
2020-10-22 15:29:00 -0500

    Index & login full

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <header class="navheader">
+        <div class="container col-md-12 col-sm-6 bg-header">
+            <div class="row">
+                <div class="col-md-3 col-sm-0 log-home">
+                    <a href="#"> <img src="img/logotipo.png" alt=""> </a>
+                </div>
+                <div class="col-md-9 col-sm-3 mr-0 p-0 navbar-dark">
+                    <nav class="nav-prin navbar navbar-expand-lg">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Perfil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Pricing</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Perfil</a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <a class="dropdown-item" href="#">Action</a>
+                                        <a class="dropdown-item" href="#">Another action</a>
+                                        <a class="dropdown-item" href="#">Contacto</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-img">
+        <div class="tex-inic">
+            <h1 class="">Abejorral Turistico</h1>
+        </div>
+    </section>
+
+    <section>
+        <br>
+        <br>
+    </section>
+
+
+    <footer class="bg-dark text-white">
+        <div class="container">
+            <nav class="row">
+                <a href="" class="col-4 text-reset text-uppercase d-flex align-items-center">
+                    <img src="img/logo-bl.png" alt="">
+                </a>
+                
+                <ul class="col-4 list-unstyled">
+                    <li class="font-weigth-bold text-uppercase">Resourse</li>
+                    <li><a href="" class="text-reset">Link 1</a></li>
+                    <li><a href="" class="text-reset">Link 1</a></li>
+                    <li><a href="" class="text-reset">Link 1</a></li>
+                </ul>
+
+                <ul class="col-4 list-unstyled">
+                    <li class="font-weigth-bold text-uppercase">quick nose</li>
+                    <li><a href="" class="text-reset">Link 1</a></li>
+                    <li><a href="" class="text-reset">Link 1</a></li>
+                    <li><a href="" class="text-reset">Link 1</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </footer>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+    <script src="vistas/js/paginacion.js"></script>
+</body>
+
+</html>

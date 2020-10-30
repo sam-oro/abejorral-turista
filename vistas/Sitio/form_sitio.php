467551c44ec9d6015f9a3e559b9db49aaf5fafee
sam-oro <samiroroz@gmail.com>
2020-10-30 09:28:59 -0500

    correciones

---
+++ vistas/Sitio/form_sitio.php
@@ -0,0 +1,30 @@
+<?php
+    require_once "../../conexion/conexion.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Agregar Sitio</h1>
+    <form action="insertar_sitio.php" method="post">
+        <fieldset>
+            <label for="nombre">Nombre del Sitio: </label><br>
+            <input type="text" name="Nombre" id="nombre" placeholder="Nombre del Sitio" required><br><br>
+            <label for="ubicacion">Ubicacion(Coordenadas): </label><br>
+            <input type="text" name="Ubicacion" id="ubicacion" placeholder="Ubicacion del Sitio"><br><br>
+            <label for="detalle">Detalles del Sitio: </label><br>
+            <textarea name="Detalle" id="detalle" cols="30" rows="10" placeholder="Detalles del Sitio"></textarea><br><br>
+            <label for="servicios">Servicios del Sitio: </label><br>
+            <textarea name="Servicios" id="servicios" cols="30" rows="10" placeholder="Servicios del Sitio"></textarea><br><br>
+            <label for="horario">Horario del Sitio: </label><br>
+            <input type="time" name="Horario" id="horario" placeholder="Horario del Sitio"><br><br>
+        </fieldset>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
--- vistas/nues-munici/nstr-mun.php
+++ vistas/nues-munici/nstr-mun.php
@@ -9,9 +9,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../../css/style.css">
 
-    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -20,7 +19,7 @@
         <div class="container col-md-12 col-sm-6 bg-header">
             <div class="row">
                 <div class="col-md-3 col-sm-0 log-home">
-                    <a href="#"> <img src="img/logotipo.png" alt=""> </a>
+                    <a href="#"> <img src="../../img/logotipo.png" alt=""> </a>
                 </div>
                 <div class="col-md-9 col-sm-3 mr-0 p-0 navbar-dark">
                     <nav class="nav-prin navbar navbar-expand-lg">
@@ -29,11 +28,11 @@
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNavDropdown">
                             <ul class="navbar-nav">
-                                <li class="nav-item active">
-                                    <a class="nav-link" href="#">Inicio<span class="sr-only"></span></a>
-                                </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Nuestro Municipio</a>
+                                    <a class="nav-link" href="#">Inicio</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Nuestro Municipio<span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">Informate</a>
@@ -63,47 +62,6 @@
         </div>
     </header>
 
-    <section class="bg-img">
-        <div class="tex-inic">
-            <h1 class="">Abejorral Turistico</h1>
-        </div>
-    </section>
-
-    <section>
-
-    </section>
-
-
-    <footer class="footer bg-dark text-white py-4">
-        <div class="container">
-            <nav class="row col-12 justify-content-center">
-                <ul class="col-sm-12 col-md-4 col-lg-4 list-unstyled">
-                    <li class="d-flex align-items-center justify-content-center mb-3 mb-md-0">
-                        <img src="img/logo-bl.png" alt="">
-                    </li>
-                </ul>
-                
-                <ul class="col-sm-12 col-md-4 col-lg-4 list-unstyled">
-                    <li class="font-weigth-bold text-uppercase">Resourse</li>
-                    <li class="d-flex align-items-center justify-content-between">
-                        <p class="ppp">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, odit voluptatibus mollitia sapiente in incidunt quam, ratione ipsam repellendus ducimus voluptate facere est. Neque iste quos quis vel perspiciatis eos.</p>
-                    </li>
-                </ul>
-
-                <ul class="col-sm-12 col-md-4 col-lg-4 list-unstyled">
-                    <li class="font-weigth-bold text-uppercase">Redes Sociales</li>
-                    <li class="red-soci d-flex justify-content-center">
-                    <a href="" class="text-reset"><i class="fab fa-twitter-square"></i></a>
-                    <a href="" class="text-reset"><i class="fab fa-facebook-square"></i></a>
-                    <a href="" class="text-reset"><i class="fab fa-instagram-square"></i></a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </footer>
-
-
-
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
@@ -111,6 +69,7 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
     <script src="vistas/js/paginacion.js"></script>
+
 </body>
 
 </html>

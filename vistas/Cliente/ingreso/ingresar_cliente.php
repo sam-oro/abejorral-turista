2178e523b4402ce7e4629e420d77845eb82168dd
Juangarzon11 <juangarzon07@gmail.com>
2020-12-04 10:47:54 -0500

    Insertar empresa y cliente listos

--- vistas/Cliente/ingreso/ingresar_cliente.php
+++ vistas/Cliente/ingreso/ingresar_cliente.php
@@ -16,7 +16,7 @@
     $contraseña=$_POST['contraseña'];
     //$Contraseña=hash("sha256", $Contraseña); QUITAR EL COMENTARIO CUANDO EL LOGIN ESTÉ LISTO
 
-    $sql="INSERT INTO tblcliente (Id_Cliente, Nombre, Apellidos, Fecha_Nacimiento, Cel, id_Municipio, Direccion, Correo, Id_Rol) VALUES ('$id', '$nombre', '$apellidos', '$fechanaci','$cel', 22 , '$direccion', '$correo', '$municipio')";
+    $sql="INSERT INTO tblcliente (Id_Cliente, Nombre, Apellidos, Fecha_Nacimiento, Cel, id_Municipio, Direccion, Correo, Id_Rol) VALUES ('$id', '$nombre', '$apellidos', '$fechanaci','$cel', '$municipio' , '$direccion', '$correo', 2 )";
 
     if ($conn->query($sql)) {
         $sql2="INSERT INTO tbllogin (Usuario, Contraseña, Id_Cliente) VALUES ('$correo', '$contraseña', '$id')";

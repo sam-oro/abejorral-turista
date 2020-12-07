4e987a90dd7ced857618dc57b00836b1cc2f49d2
Mauricio <maurox952@gmail.com>
2020-12-07 09:22:24 -0500

    modulos administrador 65%

--- admin/controlador/eliminar_cliente.php
+++ admin/controlador/eliminar_cliente.php
@@ -1,9 +1,21 @@
 <?php
 include '../../conexion/conexion.php';
+        
 
-$Id_Cliente=$_Get['Id_Cliente'];
+$Id_Cliente=$_GET['Id_Cliente'];
 
-echo $Id_Cliente;
+
+$del = $conn -> query(" DELETE FROM tbllogin WHERE Id_Cliente='$Id_Cliente'");
+
+if ($del==true){
+    $del2=$conn -> query("DELETE FROM tblcliente WHERE Id_Cliente ='$Id_Cliente'");
+    if($del2==true){
+        echo "<script> alert ('Eliminado correctamente') </script>";
+        echo "<script> 	location.href='../clientes.php'; </script>";
+    }
+}else{
+    echo "<script> 	location.href='../clientes.php'; </script>";
+}
 
 
 ?>
---
+++ admin/controlador/eliminar_empresa.php
@@ -0,0 +1,11 @@
+<?php
+include '../../conexion/conexion.php';
+        
+
+$Cod_Empresa=$_GET['Cod_Empresa'];
+
+
+echo $Cod_Empresa;
+
+
+?>

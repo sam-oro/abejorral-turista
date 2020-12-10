aaeca262996b28818a87cb97a9dc2b008ed9d25f
sam-oro <samiroroz@gmail.com>
2020-12-10 09:28:07 -0500

    Correciones formularios

--- vistas/Cliente/form_cliente.php
+++ vistas/Cliente/form_cliente.php
@@ -8,12 +8,21 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="../../css/style.css">
+
         <title>Registro Cliente</title>
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-        </script>
+
         <!--importacion boostrap-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,700;1,400&display=swap" rel="stylesheet">
+
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
+
+        <link rel="stylesheet" type="text/css" href="../../css/style.css">
+        <link rel="icon" type="image/png" href="img/icono-pag.png">
     </head>
 
     <body>

ea3d19de67b8f61e7556f3999023f0fd9cbd1550
sam-oro <samiroroz@gmail.com>
2020-12-10 16:07:03 -0500

    Vista de usuarios

---
+++ vistas/entrada_usuarios/entrada_tipousuario.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuestro Municipio</title>
+
+    <!--importacion boostrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,700;1,400&display=swap" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/f599362e7b.js" crossorigin="anonymous"></script>
+
+    <link rel="icon" type="image/png" href="img/icono-pag.png">
+
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+
+<body>
+
+    <section class="bg-usuarios" style="background-image:url(../../img/bg-tipousuario1.jpg); min-height: 50vh;">
+        <a href="">
+            <div class="p-4 p-sm-5">
+                <h1 class="text-center mb-3"> Usuario normal</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, vitae enim! Doloremque tenetur est quia omnis nisi tempore facere rem quas quisquam rerum, libero esse voluptatum enim aspernatur quis corporis magni praesentium sapiente?
+                    A ad, aspernatur qui iste quis nesciunt repudiandae blanditiis quam suscipit omnis, nostrum facilis soluta. Modi, nobis!</p>
+            </div>
+        </a>
+    </section>
+
+    <section class="bg-usuarios" style="background-image:url(../../img/bg-tipousuario2.jpg); min-height: 50vh;">
+        <a href="">
+            <div class="p-4 p-sm-5">
+                <h1 class="text-center mb-3"> Usuario normal</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, vitae enim! Doloremque tenetur est quia omnis nisi tempore facere rem quas quisquam rerum, libero esse voluptatum enim aspernatur quis corporis magni praesentium sapiente?
+                    A ad, aspernatur qui iste quis nesciunt repudiandae blanditiis quam suscipit omnis, nostrum facilis soluta. Modi, nobis!</p>
+            </div>
+        </a>
+    </section>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js " integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN " crossorigin="anonymous ">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js " integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q " crossorigin="anonymous ">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js " integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl " crossorigin="anonymous ">
+    </script>
+</body>
+
+</html>

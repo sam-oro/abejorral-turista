faf49dc07598401b8196d2247589fd64c5f3205b
PC1\asfgm <sgiraldo776@misena.edu.co>
2021-02-23 17:24:40 -0500

    favicon

--- vistas/includes/header_admin.php
+++ vistas/includes/header_admin.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" type="image/png" href="<?php echo $URL;?>img/logo-color.png">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-nav">
